0bfd6810212481e89cd7c050512d0982267c8475
Shadri <shadri.zulkurniadi@outlook.com>
2023-07-10 15:38:46 +0700

    -

--- js/script.js
+++ js/script.js
@@ -4,33 +4,6 @@ const month = String(date.getMonth() + 1).padStart(2, "0");
 const tanggal = String(date.getDate()).padStart(2, "0");
 const time = date.getTime();
 
-// This variable will save the event for later use.
-let deferredPrompt;
-
-window.addEventListener("beforeinstallprompt", function (event) {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-});
-
-// Installation must be done by a user gesture! Here, the button click
-btnAdd.addEventListener("click", (e) => {
-  // hide our user interface that shows our A2HS button
-  btnAdd.style.display = "none";
-  // Show the prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === "accepted") {
-      console.log("User accepted the A2HS prompt");
-    } else {
-      console.log("User dismissed the A2HS prompt");
-    }
-    deferredPrompt = null;
-  });
-});
-
 window.addEventListener("load", function () {
   let data = localStorage.getItem("datajadwal");
   data = JSON.parse(data);
